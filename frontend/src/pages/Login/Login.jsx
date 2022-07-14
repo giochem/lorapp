b67593e0f2706e027259920848ac621fe457a650
Xuân Trường <96410595+giochem@users.noreply.github.com>
2022-07-14 16:24:14 +0700

    Update Login.jsx

--- frontend/src/pages/Login/Login.jsx
+++ frontend/src/pages/Login/Login.jsx
@@ -1,29 +1,11 @@
 export default function Login() {
-  // developer v2
   const authGoogle = async () => {
     try {
-      window.open('http://localhost:8080/api/v2/auth/google', '_self');
+      window.open('/api/v2/auth/google', '_self');
     } catch (error) {
       console.log('🚀 ~ file: Login.jsx ~ line 51 ~ authGoogle ~ error', error);
     }
   };
-  // const getData = async () => {
-  //   try {
-  //     const config = {
-  //       headers: {
-  //         Authorization: `Bearer ${JSON.parse(sessionStorage.getItem('token'))}`,
-  //       },
-  //     };
-  //     const dataFake = await axios.get('/api/v2/auth/test-token', config);
-  //     console.log('🚀 ~ file: Login.jsx ~ line 61 ~ getData ~ dataFake', dataFake);
-  //     if (dataFake.data.accessToken) {
-  //       sessionStorage.setItem('token', JSON.stringify(dataFake.data.accessToken));
-  //     }
-  //     console.warn('🚀 ~ file: Login.jsx ~ line 56 ~ getData ~ dataFake', dataFake.data);
-  //   } catch (error) {
-  //     console.log('🚀 ~ file: Login.jsx ~ line 62 ~ getData ~ error', error);
-  //   }
-  // };
   return (
     <>
       <section className="hero is-success is-fullheight">
